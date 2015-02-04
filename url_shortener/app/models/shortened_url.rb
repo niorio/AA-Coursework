439b77be50055a99fe34dcc14a397877d1842bcf
Paul+Nick <Paul+Nick@appacademy.io>
2015-02-04 12:19:12 -0500

    Finishes Phase II.

--- url_shortener/app/models/shortened_url.rb
+++ url_shortener/app/models/shortened_url.rb
@@ -1,9 +1,33 @@
-class ShortenedURL
+class ShortenedUrl < ActiveRecord::Base
 
   validate :short_url, presence: true, uniqueness: true
   validate :long_url, presence: true, uniqueness: true
   validate :user_id, presence: true
 
+  def self.random_code
+
+    short_url = SecureRandom.urlsafe_base64
+
+    while ShortenedUrl.exists?(:short_url => short_url)
+      short_url = SecureRandom.urlsafe_base64
+    end
+
+    short_url
+
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+
+    ShortenedUrl.create!(user_id: user.id, long_url: long_url, short_url: random_code)
+
+  end
+
+  belongs_to(
+    :user,
+    :class_name => "User",
+    :foreign_key => :user_id,
+    :primary_key => :id
+  )
 
 
 end
--- url_shortener/app/models/user.rb
+++ url_shortener/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
 
   validate :email, :presence => true, :uniqueness => true
 
+  has_many(
+    :submitted_urls,
+    :class_name => "ShortenedUrl",
+    :foreign_key => :user_id,
+    :primary_key => :id
+  )
+
 end
