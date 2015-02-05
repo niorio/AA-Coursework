f145b532ad5cc342c03f13901f435f260af2b82b
Nick+Jun <Nick+Jun@appacademy.io>
2015-02-05 11:13:27 -0500

    schema and Associations I

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poller::Application.config.secret_key_base = 'beb0c542d12a9383e405b3e9863f0623ce2eb27a1bf713cc04aeebabcc0c053517143db553765b950db6974c352233fea9c0b53fc48595231fcf72fba2a20efa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poller::Application.config.session_store :cookie_store, key: '_Poller_session'
