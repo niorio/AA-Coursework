04033fd1c45dfe58f4e5c6c34c27b27d08d50dea
Nick+Jun <Nick+Jun@appacademy.io>
2015-02-05 16:26:43 -0500

    completed_polls method using ActiveRecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,34 @@ class User < ActiveRecord::Base
 
   def completed_polls
 
-    Poll.find_by_sql(
-    [
-      "SELECT
-        polls.*, COUNT(questions.*) AS question_count, COUNT(my_responses.*) AS response_count
-      FROM
-        polls
-      JOIN
-        questions ON questions.poll_id = polls.id
-      LEFT OUTER JOIN
-        (
-          SELECT
-            *
-          FROM
-            responses
-          WHERE
-            responses.user_id = ?
-        ) AS my_responses ON my_responses.question_id = questions.id
-      GROUP BY
-        polls.id
-      HAVING
-        COUNT(questions.*) = COUNT(my_responses.*)", self.id])
+    # Poll.find_by_sql(
+    # [
+      # "SELECT
+      #   polls.*, COUNT(questions.*) AS question_count, COUNT(my_responses.*) AS response_count
+      # FROM
+      #   polls
+      # JOIN
+      #   questions ON questions.poll_id = polls.id
+      # LEFT OUTER JOIN
+      #   (
+      #     SELECT
+      #       *
+      #     FROM
+      #       responses
+      #     WHERE
+      #       responses.user_id = ?
+      #   ) AS my_responses ON my_responses.question_id = questions.id
+      # GROUP BY
+      #   polls.id
+      # HAVING
+      #   COUNT(questions.*) = COUNT(my_responses.*)", self.id])
 
-
-    #self.questions.poll
-
+    Poll.find(:all,
+              :joins => "JOIN questions ON questions.poll_id = polls.id
+                        LEFT OUTER JOIN responses ON responses.question_id = questions.id",
+              :conditions =>"responses.user_id = #{id}",
+              :group => "polls.id",
+              :having=> "COUNT(questions.*) = COUNT(responses.*)")
 
 
 
