c458ec0fbb79249bfbec35a08e5f68010277c6b0
Nicholas Iorio <niorio1@gmail.com>
2015-02-06 15:59:37 -0500

    phase III

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,16 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+
+    self.all.select do |obj|
+      params.all? do |param, value|
+        obj.send(param) == value
+      end
+    end
   end
+
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,52 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.camelcase
+    @foreign_key = options[:foreign_key] || (name.to_s + "_id").to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
+    @foreign_key = options[:foreign_key] || (self_class_name.to_s + "_id").downcase.to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    #debugger
+
+    define_method(name) do
+      options.model_class.where(options.primary_key => self.send(options.foreign_key)).first
+    end
+
+
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self, options)
+
+    #debugger
+
+    define_method(name) do
+      options.model_class.where(options.foreign_key => self.send(options.primary_key))
+    end
+
   end
 
   def assoc_options
@@ -46,5 +64,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
