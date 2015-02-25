cf43cc59cb74d247849386cd0b2f8caf6bda54b1
Nick&Matt <Nick&Matt@appacademy.io>
2015-02-25 12:33:26 -0500

    Create tictactoe browser game

--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -7,12 +7,41 @@
     this.setupBoard();
     $el.append(this.board);
     this.game = game;
+    this.$el = $el
   };
 
   View.prototype.bindEvents = function () {
+    this.$el.find('.grid').on("click", "li", function(event){
+      var $cell = $(event.currentTarget);
+      var currentId = $cell.attr('id');
+      var pos = parseNum(currentId);
+
+      try {
+        this.game.playMove(pos);
+      } catch(moveError){
+        alert("Invalid Move!");
+      }
+
+      this.makeMove($cell);
+      if (this.game.winner())
+      {
+        var $winningCells = $('.' + this.game.currentPlayer);
+        $winningCells.addClass('victory');
+      }
+    }.bind(this));
   };
 
+  var parseNum = function (num)
+  {
+    var x = Math.floor(num / 3);
+    var y = num % 3;
+    return [x, y];
+  }
+
   View.prototype.makeMove = function ($square) {
+    $square.addClass('clicked');
+    $square.text(this.game.currentPlayer);
+    $square.addClass(this.game.currentPlayer);
   };
 
   View.prototype.setupBoard = function () {
@@ -21,7 +50,7 @@
 
     for (var i = 0; i < 9; i++)
     {
-      var $cell = $("<li></li>");
+      var $cell = $("<li></li>").attr('id', i);
       $grid.append($cell)
     }
     this.board = $grid;
