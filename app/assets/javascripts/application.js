678b8689c4d604c0524876c336684445f8798507
Nick + Seth <nick+seth@appacademy.io>
2015-02-27 15:44:11 -0500

    implements users search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,70 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
 //= require_tree .
+
+$.FollowToggle = function (el, options) {
+  this.$el = $(el);
+  this.userId = this.$el.data("user-id") || options.userId;
+  this.followState = this.$el.data("initial-follow-state") || options.followState;
+  this.render();
+  this.$el.on("click", this.handleClick.bind(this));
+  this.follower = options.follower;
+};
+
+$.FollowToggle.prototype.render = function () {
+  var buttonText = this.followState === "followed" ? "unfollow!" : "follow!";
+  this.$el.text(buttonText);
+};
+
+$.FollowToggle.prototype.handleClick = function(event){
+  event.preventDefault();
+
+  var actionType, newState;
+  if (this.followState === "followed"){
+    actionType = "delete";
+    newState = "unfollowed";
+  } else {
+    actionType = "post";
+    newState = "followed";
+  }
+
+  $.ajax({
+    url: "/users/" + this.userId + "/follow",
+    type: actionType,
+    dataType: "JSON",
+    success: function () {
+      this.$el.prop("disabled", false)
+      this.toggleFollower();
+      }.bind(this)
+  })
+
+  this.toggleState(newState);
+  this.render();
+  this.$el.prop("disabled", true)
+};
+
+$.FollowToggle.prototype.toggleState = function(state){
+  this.$el.data("initial-follow-state", state);
+  this.followState= state;
+}
+
+$.FollowToggle.prototype.toggleFollower = function(){
+  if (this.followState === "unfollowed"){
+    $('#user-followers > li').each(function(idx, child){
+      if ($(child).text() === this.follower) {
+        $(child).remove();
+      }
+    }.bind(this))
+  } else {
+    $('#user-followers').append("<li>"+ this.follower + "</li>");
+  }
+
+}
+
+$.fn.followToggle = function (options) {
+  return this.each(function () {
+    new $.FollowToggle(this, options);
+  });
+};
