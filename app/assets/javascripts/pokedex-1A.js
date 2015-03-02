76eae4ae312c62dc8de9686c0a01ca8a7dfcc83a
Nick&Joe <Nick&Joe@appacademy.io>
2015-03-02 11:21:22 -0500

    finish phase 1a

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,18 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  var $poke = $("<li>").text(pokemon.get("name") + ": " + pokemon.get("poke_type"));
+  $poke.addClass("poke-list-item");
+  this.$pokeList.append($poke);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
+  var that = this;
+  this.pokes.fetch({
+    success: function () {
+      that.pokes.each( function (poke) {
+        that.addPokemonToList(poke);
+      });
+    }
+  });
+
+
 };
